5d02dfccb5d2cbfa091679c03d3eafba78cf1dad
Babacar DIOP <pababacar0@gmail.com>
2024-10-15 15:59:41 +0000

    modification

--- pieces.js
+++ pieces.js
@@ -21,7 +21,13 @@ for (let i = 0; i < pieces.length; i++) {
     const descriptionElement = document.createElement("p");
     descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
     const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    if (article.disponibilite=== true) {
+        stockElement.innerText = "En Stock";
+    }else{
+        stockElement.innerText = "Rupture de Stock";
+    }
+    //Deuxieme méthode
+   // stockElement.innerText = article.disponibilite  ? "En stock" : "Rupture de stock";
     
     // On rattache la balise article a la section Fiches
     sectionFiches.appendChild(pieceElement);
@@ -33,13 +39,64 @@ for (let i = 0; i < pieces.length; i++) {
     //Ajout des éléments au DOM pour l'exercice
     pieceElement.appendChild(descriptionElement);
     pieceElement.appendChild(stockElement);
- }
+}
 
- //filtrer en fonction du prix
-const boutonTrier = document.querySelector(".btn-trier");
-boutonTrier.addEventListener("click", function () {
-    pieces.sort(function (a, b) {
-        return a.prix - b.prix;
+ //gestion des boutons 
+ const boutonTrier = document.querySelector(".btn-trier");
+ boutonTrier.addEventListener("click", function () {
+     const piecesOrdonnees = Array.from(pieces);
+     piecesOrdonnees.sort(function (a, b) {
+         return a.prix - b.prix;
+      });
+      console.log(piecesOrdonnees);
+ });
+ 
+ const boutonFiltrer = document.querySelector(".btn-filtrer");
+ boutonFiltrer.addEventListener("click", function () {
+     const piecesFiltrees = pieces.filter(function (piece) {
+         return piece.prix <= 35;
+     });
+    console.log(piecesFiltrees);
+ });
+
+ const boutonDecroissant = document.querySelector(".btn-decroissant");
+ boutonDecroissant.addEventListener("click", ()=> {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a,b) {
+        return b.prix - a.prix;
     });
-    console.log(pieces);
-});
+    console.log(piecesOrdonnees);
+ });
+
+ const boutonNonDescription = document.querySelector(".btn-nodesc");
+ boutonNonDescription.addEventListener("click", ()=>{
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description ;
+    });
+   console.log(piecesFiltrees);
+ });
+
+//Affichage de tous les pièces
+const noms = pieces.map(piece => piece.nom);
+console.log(noms);
+const prix_doubles = pieces.map(piece => piece.prix * 2);
+console.log(prix_doubles);
+//supression
+for(let i = pieces.length -1 ; i >= 0; i--){
+   if(pieces[i].prix > 35){
+       noms.splice(i,1);
+   }
+}
+
+
+//afficher la liste des pieces abordables
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+   const nomElement = document.createElement('li');
+   nomElement.innerText = noms[i];
+   abordablesElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables').appendChild(abordablesElements);
